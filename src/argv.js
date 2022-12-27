4719e59951e1cd0b061e25a27e3e346167869e22
YunRouNing <dackdada016@gmail.com>
2022-12-27 14:10:23 +0800

    process argv/env 的差異 及設定windows環境變數

---
+++ src/argv.js
@@ -0,0 +1,13 @@
+console.log(process.argv);
+
+// argv 拿到陣列
+// C:\Users\iSpan\Desktop\node.js>node src\argv.js abc 123 456
+// [
+//   1.索引值[0],拿到node
+//   'C:\\Program Files\\nodejs\\node.exe',
+//   2.索引值[1],拿到所寫的js檔及參數
+//   'C:\\Users\\iSpan\\Desktop\\node.js\\src\\argv.js',
+//   'abc',
+//   '123',
+//   '456'
+// ]
---
+++ src/list-my-env.js
@@ -0,0 +1,3 @@
+console.log(process.env.MY_VAR);
+// 設定windows的環境變數
+// SET MY_VAR= hello && node src\list-my-env.js
