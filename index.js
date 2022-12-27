c503821bc1c296662dd6666a70e76909c1b944a5
YunRouNing <dackdada016@gmail.com>
2022-12-27 15:16:17 +0800

    express app use setting res for 404 error pages

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const express = require('express');
 
 const app = express();
 
-// 路由設定,routes
+// 路由設定,routes(node都必須先setting路由)
 app.get('/',(req,res)=>{
     res.send(`<h1>hello</h1>`);
 });
+// 所有路由設定都要放在這邊之後
+// use = 接受所有http的方法
+app.use((req,res)=>{
+    res.type('text/plain');
+    res.status(404).send(`找不到你的頁面`);
+});
+
 
 const port = process.env.PORT || 3301;
  app.listen(port,()=>{
