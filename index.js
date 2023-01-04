8237ece3d9a69fdf479291a450b907295db29b30
YunRouNing <dackdada016@gmail.com>
2023-01-04 16:38:02 +0800

    登入的功能

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
   // 樣版輔助函式, helper functions
   res.locals.toDateString = d=>moment(d).format('YYYY-MM-DD');
   res.locals.toDatetimeString = d=>moment(d).format('YYYY-MM-DD HH:mm:ss');
-  
+
   res.locals.session = req.session;
 
 
@@ -207,9 +207,43 @@ app.get("/add-member", async(req, res)=>{
 app.get("/login", async(req, res)=>{
   return res.render('login');
 })
-app.post("/login", async(req, res)=>{
-  return res.json({});
+app.post("/login", upload.none(), async(req, res)=>{
+  const output = {
+    success: false,
+    code: 0,
+    errors: '',
+  };
+
+  const {email, password} = req.body;
+  if(!email || !password){
+    output.error = '欄位資料不足';
+    output.code = 400;
+    return res.json(output);
+  }
+
+  const sql = "SELECT * FROM members WHERE email=?";
+  const [rows] = await db.query(sql, [email]);
+  if(rows.length<1){
+    output.error = '帳號或密碼錯誤';
+    output.code = 410;
+    return res.json(output);
+  }
+    const row = rows[0];
+  
+    const result = await bcrypt.compare(password, row.password);
+    if(result){
+      output.success = true;
+      req.session.user = {
+        email,
+        nickname: row.nickname
+      };
+    } else {
+      output.error = '帳號或密碼錯誤';
+      output.code = 420;
+    }
+    return res.json(output);
 })
+
 app.get("/logout", async(req, res)=>{
   delete req.session.user;
   return res.redirect('/');
