5b186aadd689205c853e888fedb0e7cc4ae9a884
YunRouNing <dackdada016@gmail.com>
2023-01-04 10:32:37 +0800

    get db table value

--- routes/address-book.js
+++ routes/address-book.js
@@ -84,6 +84,69 @@ router.post("/add", upload.none(), async (req, res) => {
   res.json(output);
 });
 
+
+
+router.get("/edit/:sid", async (req, res) => {
+
+  const sid = +req.params.sid || 0;
+  if(!sid){
+    output.errors = '沒有sid';
+    return res.redirect(req.baseUrl); //轉向到列表
+  }
+  const sql = "SELECT * FROM address_book WHERE sid=?";
+  const [rows] = await db.query(sql,[sid]);
+  if(rows.length < 1){
+    output.errors = '沒有資料';
+    return res.redirect(req.baseUrl); //轉向
+  }
+
+  const row = rows[0];
+  res.json(row);
+  // res.render("ab-edit",{row});
+});
+
+router.put("/edit/:sid", upload.none(), async (req, res) => {
+  const output = {
+    success: false,
+    postData: req.body, // 除錯用
+    code: 0,
+    errors: {},
+  };
+
+  let { name, email, mobile, birthday, address } = req.body;
+
+  if(!name || name.length<2){
+    output.errors.name = '請輸入正確的姓名';
+    return res.json(output);
+  }
+
+
+  birthday = moment(birthday);
+  birthday = birthday.isValid() ? birthday.format('YYYY-MM-DD') : null;
+
+  // TODO: 資料檢查
+
+  const sql =
+    "INSERT INTO `address_book`(`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?, ?, ?, ?, ?, NOW())";
+
+  const [result] = await db.query(sql, [
+    name,
+    email,
+    mobile,
+    birthday,
+    address,
+  ]);
+
+  output.result = result;
+  output.success = !! result.affectedRows;
+
+
+  // affectedRows
+  res.json(output);
+});
+
+
+
 router.delete("/:sid", async (req, res) => {
 
   const output = {
