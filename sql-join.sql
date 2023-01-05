0d8ac8fa458422443827039b67661c356784fa01
YunRouNing <dackdada016@gmail.com>
2023-01-05 12:10:58 +0800

    LEFT OUTER JOIN

--- sql-join.sql
+++ sql-join.sql
@@ -22,3 +22,18 @@ SELECT p.*, c.name cate_name FROM `products` p JOIN
 -- 左邊的table所有資料都要呈現
 SELECT p.*, c.name cate_name FROM `products` p 
     LEFT JOIN `categories` c ON p.`category_sid` = c.sid
+
+SELECT * FROM categories c 
+  LEFT JOIN products p ON p.`category_sid`=c.sid;
+
+
+SELECT od.*, p.bookname FROM order_details od
+  JOIN products p ON od.product_sid=p.sid
+  WHERE od.order_sid=11;
+
+  -- 某個會員買過的所有商品
+
+SELECT * FROM orders o
+  JOIN order_details od ON o.sid=od.order_sid
+  JOIN products p ON od.product_sid=p.sid
+  WHERE o.member_sid=1 GROUP BY p.sid;
